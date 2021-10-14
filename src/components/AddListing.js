a4fafa295e458afdf5eb537ccaefe7d2cde9507f
Michael-Fares <mjfares@gmail.com>
2021-10-13 21:27:46 -0500

    fixed the form clearing by adding value attribue to each textfield

--- src/components/AddListing.js
+++ src/components/AddListing.js
@@ -1,8 +1,9 @@
  // should be protected by a private route
- import React, { useState } from "react";
+ import React, { useState, useEffect } from "react";
  import { Button } from "@material-ui/core";
  import { TextField } from '@material-ui/core';
  import { Box } from '@material-ui/core';
+ import { Link } from 'react-router-dom'
 
 
  const AddListing = (props) => {
@@ -12,7 +13,7 @@
     Name: '',
     Address: '',
     Hours: '',
-    Description: '',
+    Description: ''
 } )
 
   // input field change handler based on their id
@@ -30,29 +31,29 @@
     console.log("THE LISTING", payload)
     // add this.props.addCar function here
     props.addListing(payload)
+    
     // reset the form
     setListing({
       Name: '',
       Address: '',
       Hours: '',
-      Description: '',
+      Description: ''
   } )
 }
 
+
    return (
-    <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
-        <TextField id="Name" onChange={handleTextChange} placeholder="Name" />
-        <TextField id="Address" onChange={handleTextChange} placeholder="Address" />
-        <TextField id="Hours" onChange={handleTextChange} placeholder="Hours (ex 8AM-9PM)" />
-        <TextField id="Description" onChange={handleTextChange} placeholder="Description" />
+    <form className="login-form" onSubmit={handleSubmit} action="/listings">
+        <TextField value={listing.Name} required="true" id="Name" onChange={handleTextChange} placeholder="Name" />
+        <TextField value={listing.Address} required="true" id="Address" onChange={handleTextChange} placeholder="Address" />
+        <TextField value={listing.Hours} required="true" id="Hours" onChange={handleTextChange} placeholder="Hours (ex 8AM-9PM)" />
+        <TextField value={listing.Description} required="true" id="Description" onChange={handleTextChange} placeholder="Description" />
           <div className="margin-top-sm button-wide">
          <Box mt={4}>   
         <Button fullWidth = {true} 
           variant="contained"
           color="primary"
-          type="submit"
-          
-          >
+          type="submit">
           Save
         </Button>
         </Box>
