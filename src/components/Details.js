15a91007bf60060daea76d65bebf137f8a798140
Michael-Fares <mjfares@gmail.com>
2021-10-17 17:06:03 -0500

    fixed the map bug

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,7 +5,6 @@ import {
   Box
 } from '@material-ui/core';
 
-import {geocoder} from '../geolocate.js'
 
 import RoomIcon from '@material-ui/icons/Room'
 
@@ -19,14 +18,14 @@ import RoomIcon from '@material-ui/icons/Room'
 const Details = (props) => {
   const id = props.match.params.id
   const listing = props.listings.find(l => l.id == id)
-  console.log("props.listings", props)
   
   // local state for latitude and longitude 
   const [lat, setLat] = useState(0)
   const [lng, setLng] = useState(0)
   
   // using goolge geolocation API to get the lat and lng of the lising address
-  useEffect(() => {
+  const handleApiLoaded = (map, maps) => {
+    const geocoder = new window.google.maps.Geocoder() 
     geocoder.geocode({
     address: listing['Address']
   }).then((data) => {
@@ -34,24 +33,24 @@ const Details = (props) => {
      setLng(data.results[0].geometry.location.lng())
     console.log('coordinates INSIDE of goelocate', lat, lng)
   })
-  }, [])
-
-
+  }
 
   return (
     <div className="details">
        <Box><Typography variant="h5">{listing["Name"]}</Typography></Box>
        <Box mt={2}><Typography>{listing["Address"]}</Typography></Box> 
       <Box mt={2}><Typography>{listing["Hours"]}</Typography></Box>
-      <Box mt={2}><Typography>{listing["Description"]}</Typography></Box>
+      <Box mt={2} mb={2}><Typography>{listing["Description"]}</Typography></Box>
       <div style={{ height: '50vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyDXK9DB5b6QFG2gFxA4ISprrHty1zvnSWc' }}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
           defaultCenter={{
             lat: 30.2672,
             lng: -97.7431
           }}
-          defaultZoom={11}
+          defaultZoom={12}
         >
             <RoomIcon className="text-red"
             lat={lat}
--- src/geolocate.js
+++
@@ -1,2 +0,0 @@
-export const maps = window.google.maps
-export const geocoder = new window.google.maps.Geocoder() 
